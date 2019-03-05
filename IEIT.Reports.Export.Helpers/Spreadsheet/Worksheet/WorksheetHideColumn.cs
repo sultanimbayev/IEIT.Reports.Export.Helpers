7147f080ca62870b689b3b3f00a47081a3811009
Sultan Imbayev <sultan.imbayev@gmail.com>
2019-03-05 11:36:38 +0600

    Worksheet hide column feature added

---
+++ IEIT.Reports.Export.Helpers/Spreadsheet/Worksheet/WorksheetHideColumn.cs
@@ -0,0 +1,58 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Linq;
+
+namespace IEIT.Reports.Export.Helpers.Spreadsheet
+{
+    public static class WorksheetHideColumn
+    {
+
+        /// <summary>
+        /// Скрытие колонки по ее названию.
+        /// </summary>
+        /// <param name="worksheet">Лист в котором будует скрыта колонка</param>
+        /// <param name="columnName">Название колонки которая будет скрыта</param>
+        /// <returns></returns>
+        public static Worksheet HideColumn(this Worksheet worksheet, string columnName)
+        {
+            return HideColumn(worksheet, Utils.ToColumNum(columnName));
+        }
+
+        /// <summary>
+        /// Скрытие колонки по ее номеру.
+        /// </summary>
+        /// <param name="worksheet">Лист в котором будует скрыта колонка</param>
+        /// <param name="columnIndx">Номер колонки которая будет скрыта</param>
+        /// <returns></returns>
+        public static Worksheet HideColumn(this Worksheet worksheet, int columnIndx)
+        {
+            if(columnIndx < 0) { return worksheet; }
+            return HideColumn(worksheet, (uint)columnIndx);
+        }
+
+        /// <summary>
+        /// Скрытие колонки по ее номеру.
+        /// </summary>
+        /// <param name="worksheet">Лист в котором будует скрыта колонка</param>
+        /// <param name="columnIndx">Номер колонки которая будет скрыта</param>
+        /// <returns></returns>
+        public static Worksheet HideColumn(this Worksheet worksheet, uint columnIndx)
+        {
+            var columns = worksheet.GetFirstChild<Columns>();
+            Column col = null;
+            if (columns.Descendants<Column>().Any(c => c.Min == columnIndx))
+            {
+                col = columns.Descendants<Column>().Where(c => c.Min == columnIndx).First();
+            }
+            else
+            {
+                col = new Column();
+                col.Min = columnIndx;
+                col.Max = columnIndx;
+                columns.Append(col);
+            }
+            col.Hidden = true;
+            return worksheet;
+        }
+    }
+}
