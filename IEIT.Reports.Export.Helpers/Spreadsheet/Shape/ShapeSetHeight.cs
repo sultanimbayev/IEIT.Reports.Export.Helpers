36d8c1f86538ba89413aac07a8a013f0cca88f2a
Sultan Imbayev <sultan.imbayev@gmail.com>
2019-11-20 18:41:44 +0600

    0.3.8 release

--- IEIT.Reports.Export.Helpers/Spreadsheet/Shape/ShapeSetHeight.cs
+++ IEIT.Reports.Export.Helpers/Spreadsheet/Shape/ShapeSetHeight.cs
@@ -1,25 +1,24 @@
-﻿using DocumentFormat.OpenXml.Packaging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using a = DocumentFormat.OpenXml.Drawing;
 using xdr = DocumentFormat.OpenXml.Drawing.Spreadsheet;
 
 namespace IEIT.Reports.Export.Helpers.Spreadsheet
 {
-    public static class ShapeSetWidth
+    public static class ShapeSetHeight
     {
-        public static xdr.Shape SetWidthInPixels(this xdr.Shape shape, double newWidthInPixels, double dpi = 96)
+        public static xdr.Shape SetHeightInPixels(this xdr.Shape shape, double heightInPixels, double dpi = 96)
         {
-            if(shape.Parent == null) { return shape; }
+            if (shape.Parent == null) { return shape; }
             var twoCellAnchor = shape.Parent as xdr.TwoCellAnchor;
-            if(twoCellAnchor == null) {
+            if (twoCellAnchor == null)
+            {
                 var parentType = shape.Parent.GetType();
                 throw new Exception($"Parent element of shape must be TwoCellAnchor. Found \"{parentType.Name}\"");
             }
-            twoCellAnchor.SetWidthInPixels(newWidthInPixels, dpi);
+            twoCellAnchor.SetHeightInPixels(heightInPixels, dpi);
             return shape;
         }
     }
