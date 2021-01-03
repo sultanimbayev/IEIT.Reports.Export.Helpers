51114f22e7c8cb033a78cf3242990e99b98bb9f2
sultan.imbayev <sultan.imbayev@gmail.com>
2021-01-03 19:32:13 +0600

    GetDocumentId extension

--- IEIT.Reports.Export.Helpers.Tests/DocumentIdTest.cs
+++ IEIT.Reports.Export.Helpers.Tests/DocumentIdTest.cs
@@ -19,11 +19,15 @@ namespace IEIT.Reports.Export.Helpers.Tests
         {
             Do.ExcelOpen((doc) =>
             {
-                var guid = Guid.NewGuid();
-                doc.SetCustomTextProperty("myProp", guid.ToString());
-                var propValue = doc.GetCustomPropertyAsString("myProp");
+                var documentId = doc.GetDocumentId();
+                var documentId2 = doc.GetDocumentId();
+                Assert.AreEqual(documentId, documentId2);
                 var ws = doc.GetWorksheets().First();
-                ws.Write(propValue).To("B2");
+                ws.Write("Document id:").To("A2");
+                ws.Write("Document id 2:").To("A3");
+                ws.Write(documentId).To("B2");
+                ws.Write(documentId2).To("B3");
+                ws.GetColumn("A").SetWidth(20d);
             });
         }
         
---
+++ IEIT.Reports.Export.Helpers/Spreadsheet/SpreadsheetDocument/DocumentGetDocumentId.cs
@@ -0,0 +1,43 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEIT.Reports.Export.Helpers.Spreadsheet
+{
+    /// <summary>
+    /// Extension methods for generating and retrieveing id of a document
+    /// </summary>
+    public static class DocumentGetDocumentId
+    {
+        /// <summary>
+        /// Get property name for retrieving a document id
+        /// </summary>
+        /// <returns>property name for retrieving a document id</returns>
+        public static string GetDocumentIdPropertyName()
+        {
+            var ns = typeof(DocumentGetDocumentId).Namespace;
+            return ns + ".DocumentId";
+        }
+
+        /// <summary>
+        /// Get document id. Generates new document id if not exists.
+        /// </summary>
+        /// <param name="doc">A document of which to get the id.</param>
+        /// <returns></returns>
+        public static string GetDocumentId(this SpreadsheetDocument doc)
+        {
+            var documentIdPropertyName = GetDocumentIdPropertyName();
+            var documentId = doc.GetCustomPropertyAsString(documentIdPropertyName);
+            if (!string.IsNullOrWhiteSpace(documentId))
+            {
+                return documentId;
+            }
+            var newDocumentId = Guid.NewGuid().ToString();
+            doc.SetCustomTextProperty(documentIdPropertyName, newDocumentId);
+            return newDocumentId;
+        }
+    }
+}
