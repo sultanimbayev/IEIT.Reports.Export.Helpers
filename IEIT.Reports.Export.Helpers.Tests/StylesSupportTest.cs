9905cb4e0520901d900ae641e8b7b4934cdb5cdf
Sultan Imbayev <sultan.imbayev@gmail.com>
2021-01-26 19:27:02 +0600

    StylesSupport test fix

--- IEIT.Reports.Export.Helpers.Tests/StylesSupportTest.cs
+++ IEIT.Reports.Export.Helpers.Tests/StylesSupportTest.cs
@@ -4,6 +4,7 @@ using IEIT.Reports.Export.Helpers.Spreadsheet;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,20 +12,20 @@ using System.Threading.Tasks;
 namespace IEIT.Reports.Export.Helpers.Tests
 {
     [TestFixture]
-    public class GetWorkbookFromCellFormatTest
+    public class StylesSupportTest
     {
         [TestCase]
-        public void GetWorkbookFromCellFormat()
+        public void GetStylesFromAnotherWorkbook()
         {
             Do.ExcelOpen(doc =>
             {
-                
-                var stylesDocPath = @"..\Assets\StylesForTest";
+                var testDir = TestContext.CurrentContext.TestDirectory;
+                var stylesDocPath = Path.Combine(testDir, @".\Assets\StylesForTest.xlsx");
                 using (var stylesDoc = SpreadsheetDocument.Open(stylesDocPath, false))
                 {
                     var styles = stylesDoc.GetWorksheets().First().GetStyles();
                     var ws = doc.GetWorksheets().First();
-                    ws.Write("Hello").To("B2").WithStyle(styles["table_header"]);
+                    ws.Write("Hello").To("B2").WithStyle(styles["HugeText"]);
                 }
             });
         }
